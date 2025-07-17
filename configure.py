16984efdb1e5eb5e00b6c916a64455bb14c2d50d
THPengL <THPengL@outlook.com>
2025-07-17 22:41:14 +0800

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,128 +1,8 @@
 
 
 def set_default_config(args):
-    if args.dataset == 'cora':
+    if args.dataset == 'wisconsin':
         return dict(
-            datasetid = 0,
-            dataset = 'cora',
-            n_samples = 2708,
-            n_classes = 7,
-            n_edges = 5429,
-            lr = 0.0001,
-            epochs = 500,
-            dropout=0.3,
-            weight_decay = 0.01,
-            lambda1 = 0.1,
-            lambda2 = 1.0,
-            k = 0.1,
-            in_dim = 1433,
-            out_dim = 1000
-        )
-    elif args.dataset == 'citeseer':
-        return dict(
-            datasetid = 1, 
-            dataset = 'citeseer',
-            n_samples = 3327,
-            n_classes = 6,
-            n_edges = 4732,
-            lr = 0.00001,
-            epochs = 500,
-            dropout=0.3,
-            weight_decay = 0.01,
-            lambda1= 1.0,
-            lambda2 = 100.0,
-            k = 0.1,
-            in_dim = 3703,
-            out_dim = 1000
-        )
-    elif args.dataset == 'pubmed':
-        return dict(
-            datasetid = 2,
-            dataset = 'pubmed',
-            n_samples = 19717,
-            n_classes = 3,
-            n_edges = 44324,
-            lr = 0.0001,
-            epochs = 500,
-            dropout=0.3,
-            weight_decay = 0.01,
-            lambda1= 0.1,
-            lambda2 = 1.0,
-            k = 0.1,
-            in_dim = 500,
-            out_dim = 500
-        )
-    elif args.dataset == 'acm':
-        return dict(
-            datasetid = 11,
-            dataset = 'acm',
-            n_samples = 3025,
-            n_classes = 3,
-            n_edges = 13128,
-            lr = 0.0001,
-            epochs = 500,
-            dropout=0.3,
-            weight_decay = 0.01,
-            lambda1= 1.0,
-            lambda2 = 1.0,
-            k=0.1,
-            in_dim = 1870,
-            out_dim = 1000
-        )
-    elif args.dataset == 'dblp':
-        return dict(
-            datasetid = 10,
-            dataset = 'dblp',
-            n_samples = 4057,
-            n_classes = 4,
-            n_edges = 3528,
-            lr = 0.001,
-            epochs = 500,
-            dropout=0.3,
-            weight_decay = 0.01,
-            lambda1= 0.1,
-            lambda2 = 1.0,
-            k=0.1,
-            in_dim = 334,
-            out_dim = 300
-        )
-    elif args.dataset == 'texas':
-        return dict(
-            datasetid = 6,
-            dataset = 'texas',
-            n_samples = 183,
-            n_classes = 5,
-            n_edges = 309,
-            lr = 0.00001,
-            epochs = 500,
-            dropout=0.3,
-            weight_decay = 0.01,
-            lambda1 = 100,
-            lambda2 = 1.0,
-            k=0.3,
-            in_dim = 1793,
-            out_dim = 1000
-        )
-    elif args.dataset == 'cornell':
-        return dict(
-            datasetid = 7,
-            dataset = 'cornell',
-            n_samples = 183,
-            n_classes = 5,
-            n_edges = 295,
-            lr = 0.00001,
-            epochs = 500,
-            dropout=0.3,
-            weight_decay = 0.01,
-            lambda1 = 0.01,
-            lambda2 = 0.01,
-            k=0.3,
-            in_dim = 1703,
-            out_dim = 1000
-        )
-    elif args.dataset == 'wisconsin':
-        return dict(
-            datasetid = 8,
             dataset = 'wisconsin',
             n_samples = 251,
             n_classes = 5,
@@ -137,9 +17,40 @@ def set_default_config(args):
             in_dim = 1703,
             out_dim = 1000
         )
+    elif args.dataset == 'cornell':
+        return dict(
+            dataset = 'cornell',
+            n_samples = 183,
+            n_classes = 5,
+            n_edges = 295,
+            lr = 0.00001,
+            epochs = 500,
+            dropout=0.3,
+            weight_decay = 0.01,
+            lambda1 = 0.01,
+            lambda2 = 0.01,
+            k=0.3,
+            in_dim = 1703,
+            out_dim = 1000
+        )
+    elif args.dataset == 'texas':
+        return dict(
+            dataset = 'texas',
+            n_samples = 183,
+            n_classes = 5,
+            n_edges = 309,
+            lr = 0.00001,
+            epochs = 500,
+            dropout=0.3,
+            weight_decay = 0.01,
+            lambda1 = 100,
+            lambda2 = 1.0,
+            k=0.3,
+            in_dim = 1793,
+            out_dim = 1000
+        )
     elif args.dataset == 'chameleon':
         return dict(
-            datasetid = 3,
             dataset = 'chameleon',
             n_samples = 2277,
             n_classes = 5,
@@ -156,7 +67,6 @@ def set_default_config(args):
         )
     elif args.dataset == 'crocodile':
         return dict(
-            datasetid = 14,
             dataset = 'crocodile',
             n_samples = 11631,
             n_classes = 5,
@@ -171,9 +81,88 @@ def set_default_config(args):
             in_dim = 128,
             out_dim = 1000
         )
+    elif args.dataset == 'cora':
+        return dict(
+            dataset = 'cora',
+            n_samples = 2708,
+            n_classes = 7,
+            n_edges = 5429,
+            lr = 0.0001,
+            epochs = 500,
+            dropout=0.3,
+            weight_decay = 0.01,
+            lambda1 = 0.1,
+            lambda2 = 1.0,
+            k = 0.1,
+            in_dim = 1433,
+            out_dim = 1000
+        )
+    elif args.dataset == 'citeseer':
+        return dict(
+            dataset = 'citeseer',
+            n_samples = 3327,
+            n_classes = 6,
+            n_edges = 4732,
+            lr = 0.00001,
+            epochs = 500,
+            dropout=0.3,
+            weight_decay = 0.01,
+            lambda1= 1.0,
+            lambda2 = 100.0,
+            k = 0.1,
+            in_dim = 3703,
+            out_dim = 1000
+        )
+    elif args.dataset == 'pubmed':
+        return dict(
+            dataset = 'pubmed',
+            n_samples = 19717,
+            n_classes = 3,
+            n_edges = 44324,
+            lr = 0.0001,
+            epochs = 500,
+            dropout=0.3,
+            weight_decay = 0.01,
+            lambda1= 0.1,
+            lambda2 = 1.0,
+            k = 0.1,
+            in_dim = 500,
+            out_dim = 500
+        )
+    elif args.dataset == 'acm':
+        return dict(
+            dataset = 'acm',
+            n_samples = 3025,
+            n_classes = 3,
+            n_edges = 13128,
+            lr = 0.0001,
+            epochs = 500,
+            dropout=0.3,
+            weight_decay = 0.01,
+            lambda1= 1.0,
+            lambda2 = 1.0,
+            k=0.1,
+            in_dim = 1870,
+            out_dim = 1000
+        )
+    elif args.dataset == 'dblp':
+        return dict(
+            dataset = 'dblp',
+            n_samples = 4057,
+            n_classes = 4,
+            n_edges = 3528,
+            lr = 0.001,
+            epochs = 500,
+            dropout=0.3,
+            weight_decay = 0.01,
+            lambda1= 0.1,
+            lambda2 = 1.0,
+            k=0.1,
+            in_dim = 334,
+            out_dim = 300
+        )
     elif args.dataset == 'photo':
         return dict(
-            datasetid = 12,
             dataset = 'photo',
             n_samples = 7650,
             n_classes = 8,
@@ -191,7 +180,6 @@ def set_default_config(args):
     else:
         print("Using Default Config")
         return dict(
-            datasetid=15,
             dataset=args.dataset,
             n_samples=2000,
             n_classes=10,
